5917d314198fc92c0c5011037cd40b19b18d5665
Srijan R Shetty <srijan.shetty@gmail.com>
2013-04-15 14:19:40 +0530

    The uploaded version

--- crawl.py
+++ crawl.py
@@ -1,6 +1,6 @@
 # To get the HTML of the website
 import urllib2
-from BeautifulSoup import BeautifulSoup
+from bs4 import BeautifulSoup
 import re
 from urlparse import urljoin, urlparse
 import sys,traceback
@@ -13,9 +13,6 @@ from google import search
 processed_urls=[]
 blacklist_urls=[]
 deadline_urls=[]
-
-# File to store the urls
-processed_urls_file=open('processed_urls','w')
 
 def getHTML(link):
 	"""Extract HTML from a page and return HTML text
@@ -45,20 +42,43 @@ def getDeadline(soup,link):
 	"""Extracts deadline from a given soup element
 		Arguments: soup object, link
 	"""
-	
-	text=soup.findAll(text=True) # Extract text from the soup object
+	text = soup.findAll(text=True)
+	flag=0
+	flag_date=0
 	for line in text:
-		deadline=re.findall(r'[Ss]ubmission\s[Dd]eadline.*\d.*', str(line)) # Find all strings matching deadline
+		prev_line = line
+		deadline=re.search(r'(.*submission\sdeadline*.*)|(.*deadline\ssubmission*.*)|(.*deadline)|(.*notification of acceptance)|(.*important dates.*)', str(line), re.IGNORECASE) # Find all strings matching deadline
+		date=re.search(r'(^(0[1-9]|[12][0-9]|3[01])[- /.](0[1-9]|1[012])[- /.](19|20)\d\d)|(.*January\s*.*)|(.*February\s*.*)|(.*March\s*.*)|(.*April\s*.*)|(.*May\s*.*)|(.*June\s*.*)|(.*July\s*.*)|(.*August\s*.*)|(.*September\s*.*)|(.*October\s*.*)|(.*November\s*.*)|(.*December\s*.*)',str(line),re.IGNORECASE)
+		if(flag_date):
+			due_date=re.search(r'.*[Dd]ue.*',str(line),re.IGNORECASE)
+			if(due_date):
+				print due_date
+			#	print prev_line.string
+			flag_date=0
 		if (deadline):
 			# Now we get the base url from the link and add this entry into the database
+			flag=1
+			flag2=re.search(r'(^(0[1-9]|[12][0-9]|3[01])[- /.](0[1-9]|1[012])[- /.](19|20)\d\d)|(.*January\s*.*)|(.*February\s*.*)|(.*March\s*.*)|(.*April.*)|(.*May\s*.*)|(.*June\s*.*)|(.*July\s*.*)|(.*August\s*.*)|(.*September\s*.*)|(.*October\s*.*)|(.*November\s*.*)|(.*December\s*.*)',str(deadline.group()),re.IGNORECASE)
 			base_url=urlparse(link).netloc
-			print base_url, deadline
+			print base_url, deadline.group()
+		if(date):
+			flag_date=0
+			if(flag):
+				if(flag2):
+					pass
+				else:
+					base_url=urlparse(link).netloc
+					print base_url, date.group()
+					flag_date=1
+					if(flag>=1):
+						flag=0
+					else:	
+						flag=flag+1
 
 def crawl(main_link,depth):
 	"""Extracts HTML from a URL, and gets the links from them
 	Arguments: link, depth_crawling
 	"""
-
 	# Create a soup and check for HTML Errors
 	HTML=getHTML(main_link)
 	if(HTML):
@@ -75,7 +95,7 @@ def crawl(main_link,depth):
 	  	return None
 
 	# Checking for depth
-	if(depth==0):
+	if(depth<=0):
 		return
 
 	# Processing the links
@@ -86,9 +106,12 @@ def crawl(main_link,depth):
 		urls=re.findall(r'href=["]([^"]*)["]','%s' %link)  # Regex for the URL 
 		for url in urls:
 			temp_url=urljoin(main_link,url)
-			if(temp_url not in processed_urls and temp_url not in blacklist_urls): # Check if the URL is in processed list
+			if(urlparse(temp_url).netloc in blacklist_urls): # Check if the URL is not in blacklist
+				print '\nSkipping URL: %s' %temp_url
+			elif(re.findall(r'.*[.](pdf)|(zip)', str(temp_url))):
+				print '\nSkipping URL: %s' %temp_url
+			elif(temp_url not in processed_urls):
 				processed_urls.append(temp_url)
-				processed_urls_file.write("%s\n" %temp_url)
 				print '\nCrawling URL: %s' %temp_url
 				crawl(temp_url,depth-1) # Search for more links
 	
@@ -125,9 +148,9 @@ def frontier():
 		for url in google_search:
 			if(urlparse(url).netloc in blacklist_urls):
 				print "\nSkipping URL: %s" %url
-				processed_urls.append(url)
 			else:
 				print "\nCrawling URL: %s" %url
+				processed_urls.append(url)
 				crawl(url,depth)
 	except urllib2.HTTPError, e:
 	    print 'The server couldn\'t fulfill the request.'
@@ -135,8 +158,6 @@ def frontier():
 	except urllib2.URLError, e:
 		print 'We failed to reach a server.'
 		print 'Reason: ',e.reason
-
-			
 
 if __name__ == '__main__':
 	
